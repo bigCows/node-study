4207f792179274dc051d6b751f9e11e2d06e1cdc
mxf <547087214@qq.com>
2023-03-28 13:59:41 +0800

    init-Project

---
+++ http.js
@@ -0,0 +1,48 @@
+var http = require('http')
+const PORT = 3001
+const service = http.createServer()
+var url = require('url')
+console.log(url);
+
+service.on('request',(req,res) => {
+
+  if(url === 'favicon.ico') {
+    return ''
+  }
+  console.log(url.parse(req.url));
+  let pathName = url.parse(req.url).pathname
+
+  // get请求参数
+  let queryString = url.parse(req.url,true).query
+  console.log('queryString ',queryString)
+  if(queryString) {
+    console.log('有参数');
+    console.log(queryString.a);
+    console.log(queryString.b);
+  }
+  
+  res.setHeader('content-type','text/html;charset=utf-8')
+  const isPage = pageUrl(pathName)
+  res.write(isPage)
+  res.end()
+
+});
+
+service.listen(PORT,() => {
+  console.log(PORT + '端口已启动');
+})
+
+let urlArr = ['/list','/home']
+
+const pageUrl = (url) => {
+  if(urlArr.includes(url) > 0) {
+    return `<html><h1>${url}页面</h1></html>`
+  } else {
+    return '<html><h1>404 NOT FOUND</h1></html>'
+  }
+}
+
+const a = url.resolve('qwea/aaa/xxx/ddd','ccc')
+const b = url.resolve('http://www.baidu.com/qwea/aaa/xxx/ddd','ccc')
+console.log(a);
+console.log(b);
---
+++ url.js
@@ -0,0 +1,42 @@
+let url = require('url')
+let http = require('http')
+
+
+const DOMAIN = 'http://www.baidu.com'
+const PORT = 3002
+const service = http.createServer()
+
+/**
+ * nodeJS中文网: https://nodejs.cn/api-v16/url.html#urlformaturl-options
+ */
+
+service.on('request',(req,res) => {
+  // let queryString = url.parse(req.url).query  // 获取路径参数
+  // let objParams = url.parse(req.url,true).query // 将路径参数转为键值对
+  // let pathName = url.parse(req.url).pathname // 获取前端请求的接口
+  // let joinUrl = url.resolve(DOMAIN,'ccc') // 前面有斜杠（'/'）,它将替换域名之后的整个路径。前面没有斜杠（'/'）,它将替换基本域名中斜杠之后的所有内容，不是域名有斜杠直接拼接，无斜杠，替换最后一个斜杠后的所有内容
+  res.setHeader('content-Type','text/html;charset=utf-8')
+  res.write('hello,nodeJS')
+  let myUrl = new URL(req.url,'http://localhost:3002')
+  for (const [key,val] of myUrl.searchParams) {
+    // console.log(key);
+    // console.log(val);
+  }
+  // console.log('myUrl ',myUrl)
+  if(myUrl.pathname === '/home') {
+    res.write('<html>home页面</html>')
+  }
+  const myURL = new URL('https://a:b@測試?abc#foo');
+  console.log(myURL.href);
+  console.log(url.format(myURL,{fragment:false}));
+  // auth: url中是否显示用户名和密码，默认true;
+  // fragment: url是否显示 代码片段(#后的)，默认true
+  // search: 是否显示路径参数,默认true
+  // unicode：是否采用unicode编码，默认false
+
+  res.end()
+})
+
+service.listen(PORT,() => { console.log(PORT + '端口已启动');})
+
+
