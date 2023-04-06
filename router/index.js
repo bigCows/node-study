1d3bc172e57e352c047b9d02a642083ccaadadf9
mxf <547087214@qq.com>
2023-04-06 17:14:51 +0800

    统一处理静态资源

--- router/index.js
+++ router/index.js
@@ -4,15 +4,24 @@
  */
 
 const fs = require('fs')
+const path = require('path')
+const mime = require('mime')
 
 const router = {
+  '/': (req,res) => {
+    render('./static/login.html',res)
+  },
   '/login': (req,res) => {
     render('./static/login.html',res)
   },
   '/home': (req,res) => {
     render('./static/home.html',res)
   },
+  '/api': (req,res) => {
+    render('./static/api.html',res)
+  },
   '/404': (req,res) => {
+    if(readStatic(req,res)) return
     render('./static/404.html',res)
   },
   '/favicon.ico': (req,res) => {
@@ -25,5 +34,18 @@ const render = (path,res,type="") => {
   res.write(fs.readFileSync(path))
   res.end()
 }
+
+// 静态资源处理
+const readStatic =(req,res) => {
+  const myUrl = new URL(req.url,'http://127.0.0.1:3000').pathname
+  const pName = path.join(path.resolve(),myUrl)
+  if(fs.existsSync(pName)) {
+    console.log('ininin');
+    render(pName,res,mime.getType(path.extname(myUrl)))
+    return true
+  } else {
+    return false
+  }
+} 
 
 module.exports = router
