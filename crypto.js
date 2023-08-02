cc5df6fc57b2f91326fa998677f9a8b21df43630
mxf <547087214@qq.com>
2023-08-02 16:54:03 +0800

    添加描述

--- crypto.js
+++ crypto.js
@@ -3,16 +3,21 @@
  */
 
 const crypto = require('crypto')
+const path = require('path')
 
 // const hash = crypto.createHash('sha1')
 const hash = crypto.createHash('md5')
+console.log(hash,'hash');
+
 // 通过使用秘钥加强算法安全性
 const Hmac = crypto.createHmac('sha1','mxf')
+
 // 获取所有加密算法key
 const t = crypto.getHashes()
-// console.log(t);
+console.log(t);
 
-hash.update('asdaf')
+const hashStr = hash.update('asdaf')
+console.log(hashStr,'hashStr');
 hash.update('ddfgdfgdf')
 
 const hex = hash.digest('hex')
@@ -27,6 +32,14 @@ Hmac.update('mmmmmm')
 
 // 生成16位随机数
 const randomNum = String(Math.random() * Math.pow(10,16)).slice(0,16)
+console.log(randomNum,'randomNum');
+
+// 获取文件后缀名
+const extname = path.extname('index.html').slice(1)
+console.log(extname,'extname');
+
+const suffix = 'asdada.jsx'
+console.log(suffix.substring(suffix.lastIndexOf('.') + 1));
 
 const SECRET_KEY = 'mxf+02$485@91-=='
 let iv = 'asi2!va3=2dl5kd2'
@@ -34,16 +47,18 @@ let isData = '哈哈哈哈哈哈啊'
 
 /**
  * @method encrypt
+ * @param {any} data
+ * @param {string} secertKey
  * @description 加密算法
  * @data 要加密数据
  * @key 秘钥
  * @iv 加密算法的初始向量
  */
-const encrypt = (data,secertKey,iv) => {
+const encrypt = (data,secertKey) => {
   let ciper = crypto.createCipheriv("aes-128-cbc",secertKey,iv)
   let encryted = ciper.update(data,'utf-8','hex')
   encryted += ciper.final('hex')
-  console.log(encryted,'cc');
+  console.log(encryted,'加密');
   return encryted
 
 }
@@ -53,12 +68,11 @@ const isEncrypted = encrypt(isData,SECRET_KEY,iv)
 
 /**
  * 
- * @param {string} data 
+ * @param {any} data 
  * @param {string} secertKey 
- * @param {string} iv 
- * @description 解密暂时有误
+ * @description 解密算法
  */
- const decrypt = (data, secretKey, iv) => {
+ const decrypt = (data, secretKey) => {
   // 对密文进行 Base64 解码
   const buf = Buffer.from(data, 'hex').toString('binary');
   let dep = crypto.createDecipheriv('aes-128-cbc', secretKey, iv);
@@ -68,6 +82,6 @@ const isEncrypted = encrypt(isData,SECRET_KEY,iv)
   // console.log(res2, 'res2');
   // console.log(res+res2, 'res2');
 }
-  const decryptStr = decrypt(isEncrypted,SECRET_KEY,iv)
-  console.log(decryptStr,'decryptStr');
+  const decryptStr = decrypt(isEncrypted,SECRET_KEY)
+  console.log(decryptStr,'解密');
 
--- path.js
+++ path.js
@@ -1,5 +1,14 @@
 const path = require('path')
 
+
+/**
+ * 
+ * path.resolve() 方法会把一个路径或路径片段的序列解析为一个绝对路径。
+ * @description 如果没有传入 path 片段，则 path.resolve() 会返回当前工作目录的绝对路径,传入参数会将参数解析为绝对路径
+ * 如果最后一个参数第一位是/，则会忽略前面的参数，直接返回最后一个参数
+ * 如果最后一个参数第一位不是/，则会将前面的参数都在绝对路径后拼接起来，然后返回
+ */
+
 const url = path.resolve('a/b','src/components')
 // const url = path.resolve('')
 console.log(url);
@@ -17,7 +26,7 @@ const res = path.join(__dirname, 'src/components')
 const pt = path.resolve()
 console.log(pt,'pt');
 // const u = path.resolve('wwwroot', './static_files/aaa/bbb', '/gif/image.gif');
-// const u = path.resolve('wwwroot', '../static_files/aaa/bbb', '/gif/image.gif');
-const u = path.resolve('wwwroot', '/static_files/aaa/bbb', '../gif/image.gif');
+const u = path.resolve('wwwroot', '../static_files/aaa/bbb', '/gif/image.gif');
+// const u = path.resolve('wwwroot', '/static_files/aaa/bbb', '../gif/image.gif');
 // const u = path.resolve('/static_files/aaa/bbb', 'gif/image.gif');
 console.log(u,'u');
